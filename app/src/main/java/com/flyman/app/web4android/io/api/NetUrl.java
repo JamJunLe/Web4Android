93fb09a93360061973e082820475fbf417ac5980
flymanmolo <flymanmolo@gmail.com>
2017-10-21 13:46:07 +0800

    添加"关于"菜单

--- app/src/main/java/com/flyman/app/web4android/io/api/NetUrl.java
+++ app/src/main/java/com/flyman/app/web4android/io/api/NetUrl.java
@@ -2,4 +2,5 @@ package com.flyman.app.web4android.io.api;
 
 public interface NetUrl {
     String BASE_URL = "http://www.jcodecraeer.com";//泡在网上的日子主页
+    String GITHUB_HOMEPAGE = "https://github.com/JamJunLe/Web4Android";//GitHub地址首页
 }
